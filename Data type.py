29d3ecc971d34e3b5dde84ec204005fc29f33a1f
Chethan A C <acchethan15@gmail.com>
2024-10-02 13:28:46 +0530

    Create Data type.py

---
+++ Data type.py
@@ -0,0 +1,12 @@
+a=2;
+b=True;
+c=False;
+d="Chethan";
+e=None;
+f=complex(2,3);
+print("The type is",type(a));
+list1=[1,2,3];
+print(list1);
+tupple1={"apple","bananna"};
+print(tupple1);
+dict1{"Name":"Rocky","Since":1951,"Gangster":True;
