2fb8b6e60f4db3918335059c5685d211ae375669
Chethan A C <acchethan15@gmail.com>
2024-10-20 23:55:30 +0530

    Update Data type.py

--- Data type.py
+++ Data type.py
@@ -7,6 +7,6 @@ f=complex(2,3);
 print("The type is",type(a));
 list1=[1,2,3];
 print(list1);
-tupple1={"apple","bananna"};
+tupple1={"apple","banana"};
 print(tupple1);
 dict1{"Name":"Rocky","Since":1951,"Gangster":True;
