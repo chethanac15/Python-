9fad2294fe11cf8d3effbf083697542bca48d5f7
Chethan A C <acchethan15@gmail.com>
2025-02-19 12:25:03 +0530

    Create if-else2.py

---
+++ if-else2.py
@@ -0,0 +1,7 @@
+a = int(input("Enter your age"))
+b = str(input("Enter ypur name"))
+print("Your name is",b,"and your age is",a)
+if a>=18:
+    print("You can vote")
+else:
+    print("You cant vote")
